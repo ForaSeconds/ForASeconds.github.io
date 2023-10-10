0632406a6df61fca69f10067468217273889f5ef
ForASeconds <zhenya343@ukr.net>
2023-10-10 19:23:16 +0200

    1

--- main.js
+++ main.js
@@ -8,8 +8,10 @@ changeBackgroundButton.addEventListener('click', () => {
 
 
 function getRandomColor() {
-    const letters = "0123456789ABCDEF";
+    const letters = '0123456789ABCDEF';
     let color = '#';
-    for (let i = 0; 1 < 6; i++); {color += letters[Math.floor(Math.random() * 16)]}
-    return color
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
